441d05f2497d3b4573ad99d8f705e9ca94e9a31e
hyeongiii <hyeongiii0811@gmail.com>
2023-05-17 21:51:43 +0900

    :sparkles: feat: 서블릿 컨테이너 생성

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,19 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		// 톰캣 서블릿 웹서버의 설정 및 생성을 도와주는 클래스
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		// 웹 서버(서블릿 컨테이너) 생성 함수 : getWebServer()
+		WebServer webServer = serverFactory.getWebServer();
+		// 톰캣 서블릿 컨테이너 동작
+		webServer.start();
 	}
 
 }
+
+// 서블릿 컨테이너 대표적 예시 : 톰캣
