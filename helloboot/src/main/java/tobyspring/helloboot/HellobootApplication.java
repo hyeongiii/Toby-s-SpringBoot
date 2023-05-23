56b0698cf4a7013635f24686f7bd9ee16455afe1
hyeongiii <hyeongiii0811@gmail.com>
2023-05-23 23:57:20 +0900

    :sparkles: feat: 서블릿 요청 처리

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,10 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +20,14 @@ public class HellobootApplication {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		// 웹 서버(서블릿 컨테이너) 생성 함수 : getWebServer(new ServletContextInitializer() {})
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			// 익명 클래스의 람다식 전환
+			// 익명 클래스의 람다식 전환 / 서블릿 컨테이너에 웹 요청을 처리하는 서블릿 등록
 			servletContext.addServlet("hello", new HttpServlet() {
-				// 서블릿 컨테이너에 웹 요청을 처리하는 서블릿 등록
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					resp.setStatus(200);
-					resp.setHeader("Content-Type", "text/plain");
+					String name = req.getParameter("name");
+
+					resp.setStatus(HttpStatus.OK.value());		// 하드코딩(직접 작성)보다는 spring이 제공하는 ENUM으로!
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
 					resp.getWriter().print("Hello Servlet");
 				}
 			}).addMapping("/hello");
