e24251949dc5c2990b3599cb26fe1e53418de8a9
hyeongiii <hyeongiii0811@gmail.com>
2023-05-23 23:51:59 +0900

    :sparkles: feat: 서블릿 컨테이너에 서블릿 등록

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,14 +2,32 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// 톰캣 서블릿 웹서버의 설정 및 생성을 도와주는 클래스
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		// 웹 서버(서블릿 컨테이너) 생성 함수 : getWebServer()
-		WebServer webServer = serverFactory.getWebServer();
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		// 웹 서버(서블릿 컨테이너) 생성 함수 : getWebServer(new ServletContextInitializer() {})
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			// 익명 클래스의 람다식 전환
+			servletContext.addServlet("hello", new HttpServlet() {
+				// 서블릿 컨테이너에 웹 요청을 처리하는 서블릿 등록
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().print("Hello Servlet");
+				}
+			}).addMapping("/hello");
+
+		});
 		// 톰캣 서블릿 컨테이너 동작
 		webServer.start();
 	}
