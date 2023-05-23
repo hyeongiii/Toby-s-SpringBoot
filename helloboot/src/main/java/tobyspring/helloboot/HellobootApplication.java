3d2002ed16a92666909d2e436d8e890634f11e72
hyeongiii <hyeongiii0811@gmail.com>
2023-05-24 00:05:59 +0900

    :recycle: refactor: 서블릿을 프론트 컨트롤러로 전환

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -21,16 +22,26 @@ public class HellobootApplication {
 		// 웹 서버(서블릿 컨테이너) 생성 함수 : getWebServer(new ServletContextInitializer() {})
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			// 익명 클래스의 람다식 전환 / 서블릿 컨테이너에 웹 요청을 처리하는 서블릿 등록
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontcontroller", new HttpServlet() {    // front-controller : 모든 요청의 중앙 처리/ 서블릿 컨테이너의 mapping 기능 수행
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					// 인증, 보안, 다국어, 공통 기능 처리
 
-					resp.setStatus(HttpStatus.OK.value());		// 하드코딩(직접 작성)보다는 spring이 제공하는 ENUM으로!
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().print("Hello Servlet");
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+
+						resp.setStatus(HttpStatus.OK.value());		// 하드코딩(직접 작성)보다는 spring이 제공하는 ENUM으로!
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().print("Hello Servlet");
+					}
+					else if (req.getRequestURI().equals("/user")) {
+						//
+					}
+					else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 
 		});
 		// 톰캣 서블릿 컨테이너 동작
