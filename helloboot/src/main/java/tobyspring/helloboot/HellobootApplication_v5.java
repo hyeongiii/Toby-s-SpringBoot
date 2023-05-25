4e18a9afc0e58f54aea68387c5bace88b2d817f2
hyeongiii <hyeongiii0811@gmail.com>
2023-05-25 23:46:44 +0900

    :sparkles: feat: 자바코드로 config 구성 정보 사용

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication_v5.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication_v5.java
@@ -3,15 +3,34 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * Spring Container 를 초기화하는 과정에서 dispatcherServlet 을 초기화하도록 통합 (SpringBoot 의 동작 방식)
+ *
+ * @Configuration : BeanFactory Method(@Bean 애노테이션이 붙은 메서드)가 있다고 spring container 에게 정보를 제공
+ * -> spring container 은 @Configuration 애노테이션이 붙은 클래스에서 @Bean 이 붙은 메서드를 찾아 빈 object 를 생성한다.
  */
+@Configuration
 public class HellobootApplication_v5 {
+
+    @Bean
+    public HelloServletController helloServletController(HelloService helloService) {
+        return new HelloServletController(helloService);
+    };
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
+
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        // 애노테이션으로 config 정보를 읽을 수 있는 spring container
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -26,8 +45,7 @@ public class HellobootApplication_v5 {
                 webServer.start();
             }
         };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(HellobootApplication_v5.class);
         applicationContext.refresh();
 
 
